8fdb15c02e6bd09345cf8305f85211a9b3a7ffaf
Angel Perez <perryplatipus4@gmail.com>
2025-10-11 01:39:22 -0700

    Deck Creation Implemented

--- MidtermProject.java
+++ MidtermProject.java
@@ -4,23 +4,36 @@ import java.util.Random;
 public class MidtermProject {
     public static void main(String[] args) {
         ArrayList<Object> Deck = new ArrayList<>();
-        Deck.add(0);
-
+        NewDeck(Deck);
+        System.out.println(Deck);
     }
     public static void NewDeck(ArrayList<Object> Deck){
         Deck.clear();
         String Suit = "";
-        for (int j = 0; j < 4; j++){
-            if (j == 0)
+        String Face = "";
+        for (int j = 1; j <= 4; j++){
+            if (j == 1)
                 Suit = "Diamonds";
-            else if (j == 1)
-                Suit = "Clubs";
             else if (j == 2)
-                Suit = "Hearts";
+                Suit = "Clubs";
             else if (j == 3)
+                Suit = "Hearts";
+            else if (j == 4)
                 Suit = "Spades";
-            for (int i = 0; i < 9; i++)
-                Deck.add(i + " of " + Suit);
+            for (int k = 1; k <= 3; k++){
+                if (k == 1)
+                    Face = "Jack";
+                else if (k == 2)
+                    Face = "Queen";
+                else if (k == 3)
+                    Face = "King";
+                Deck.add(Face + " of " + Suit);
+            }
+            for (int i = 1; i <= 10; i++)
+                if (i == 1)
+                    Deck.add("Ace of " + Suit);
+                else
+                    Deck.add(i + " of " + Suit);
         }
     }
 }
