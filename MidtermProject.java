f4aee40529be39c636d3f9838fa29b49ab9e55d9
Angel Perez <perryplatipus4@gmail.com>
2025-10-11 01:09:26 -0700

    Commit 1

--- MidtermProject.java
+++ MidtermProject.java
@@ -4,14 +4,24 @@ import java.util.Random;
 public class MidtermProject {
     public static void main(String[] args) {
         ArrayList<Object> Deck = new ArrayList<>();
-        Deck.add("Lebron");
-        Deck.add(1);
-        System.out.println(Deck.get(0));
-        System.out.println(Deck.get(1));
-    }
-    public static int NewDeck(){
-        Deck.clear();
+        Deck.add(0);
 
+    }
+    public static void NewDeck(ArrayList<Object> Deck){
+        Deck.clear();
+        String Suit = "";
+        for (int j = 0; j < 4; j++){
+            if (j == 0)
+                Suit = "Diamonds";
+            else if (j == 1)
+                Suit = "Clubs";
+            else if (j == 2)
+                Suit = "Hearts";
+            else if (j == 3)
+                Suit = "Spades";
+            for (int i = 0; i < 9; i++)
+                Deck.add(i + " of " + Suit);
+        }
     }
 }
 
