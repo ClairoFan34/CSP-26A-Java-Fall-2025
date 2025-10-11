b0e3764c6bfbc9d4f6cca17c7393d2161174b497
Angel Perez <perryplatipus4@gmail.com>
2025-10-11 00:27:26 -0700

    Add files via upload

---
+++ CSP 26A Java/MidtermProject.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MidtermProject {
+    public static void main(String[] args) {
+        ArrayList<Object> Deck = new ArrayList<>();
+        Deck.add("Lebron");
+        Deck.add(1);
+        System.out.println(Deck.get(0));
+        System.out.println(Deck.get(1));
+    }
+    public static int NewDeck(){
+        Deck.clear();
+
+    }
+}
