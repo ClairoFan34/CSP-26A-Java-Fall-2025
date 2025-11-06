e63c2106779fc99d7ececc665e1174b68b25e753
Angel Perez <perryplatipus4@gmail.com>
2025-11-06 15:56:03 -0800

    Week 10-11 Lab completed

--- AttributesProject/faculty.java
+++ AttributesProject/faculty.java
@@ -1,5 +1,4 @@
 package AttributesProject;
-
 public class faculty {
     
 }
---
+++ In-Class Labs/Week10_11InClassLab.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Week10_11InClassLab {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Calls checkExam and getStuAnswers
+        checkExam(getStuAnswers());
+    }
+
+    // checkExam is used to compare the given list of answers determined in getStuAnswers
+    // Correct answers are hard-coded
+    // Amount of answers incorrect are tracked by decrementing answers correct when the two arrays dont match, starting at 20
+    // Index is also added to an arraylist, to track what exact question were wrong
+    public static void checkExam(char[] studentAnswers) {
+        ArrayList<Integer> answersWrong = new ArrayList<>();
+        int answersCorrect = 20;
+        int i = 0;
+        char[] correctAnswers = {'b','d','a','a','c','a','b','a','c','d','b','c','d','a','d','c','c','b','d','a'};
+        for (i = 0; i < correctAnswers.length; i++){
+            if (correctAnswers[i] != studentAnswers[i]){
+                answersWrong.add(i);
+                answersCorrect--;
+            }
+        }
+        System.out.println("Total score: " + answersCorrect + " / 20");
+        System.out.println("Questions wrong: " + answersWrong);
+        if (answersCorrect >= 15)
+            System.out.println("You passed!");
+        else
+            System.out.println("You failed!");
+    }
+
+    public static char[] getStuAnswers() {
+        char [] studentAnswers = new char[20];
+        System.out.println("Exam Start!");
+        for (int i = 0; i < studentAnswers.length;){
+            System.out.println("Enter your answer (A,B,C,D) - Question " + (i + 1));
+            studentAnswers[i] = input.nextLine().toLowerCase().charAt(0);
+            switch (studentAnswers[i]){
+                case 'a':
+                    i++;
+                    break;
+                case 'b':
+                    i++;
+                    break;
+                case 'c':
+                    i++;
+                    break;
+                case 'd':
+                    i++;
+                    break;
+                default:
+                    System.out.println("Only characters A through D are accepted!!!");
+                    break;
+            }
+        }
+        return studentAnswers;
+    }
+}
