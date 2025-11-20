6d5bbede3616ead533845d19ee65b2a0e5a6935b
Angel Perez <perryplatipus4@gmail.com>
2025-11-19 21:14:33 -0800

    Week 12 Project Upload

---
+++ In-Class Labs/Week 12-13 In-Class Lab/CellPhone.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class CellPhone {
+    private String brand;
+    private String model;
+    private int storage;
+    private boolean isOn;
+    private double batteryLevel;
+    private ArrayList<String> contacts = new ArrayList<>();
+    
+}
