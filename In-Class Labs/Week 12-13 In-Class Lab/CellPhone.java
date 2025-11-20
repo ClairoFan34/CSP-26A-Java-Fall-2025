b59da69f7ad4713bd55c09b9478e3109353888cc
Angel Perez <perryplatipus4@gmail.com>
2025-11-19 23:08:43 -0800

    All functionality methods done -___- finally!

--- In-Class Labs/Week 12-13 In-Class Lab/CellPhone.java
+++ In-Class Labs/Week 12-13 In-Class Lab/CellPhone.java
@@ -6,7 +6,7 @@ public class CellPhone {
     private String model;
     private int storage;
     private boolean isOn;
-    private int batteryLevel;
+    private double batteryLevel;
     private ArrayList<String> contacts = new ArrayList<>();
 
     // Constructor 1: Default data constructor, initializes all variables when creating a new object
@@ -51,7 +51,77 @@ public class CellPhone {
         this.storage = storage;
     }
     //Battery
-    public int getBattery() {
+    public double getBattery() {
         return batteryLevel;
+    }
+    // Contacts
+    public ArrayList<String> getContacts(){
+        return contacts;
+    }
+    // Functionallity methods
+    // useBattery: helper method called whenever a certain action is made, depletes the amount of battery depending on action
+    private void useBattery(double amount) {
+        batteryLevel -= amount;
+        if (batteryLevel < 0) {
+            batteryLevel = 0;
+        }
+    }
+    // chargeBattery: Just fills the battery back up to full
+    public void chargeBattery() {
+        batteryLevel = 100;
+        System.out.println("Battery fully charged.");
+    }
+    // togglePower: checks if phone is on or off and does the opposite actions, powering on drains a little battery as well
+    // If the battery is fully depleted, it cannot be powered on
+    public void togglePower(){
+        if (isOn == true) {
+            System.out.println("Phone is turning off...");
+            isOn = false;
+        } 
+        else {
+            if (batteryLevel <= 0) {
+                System.out.println("Battery dead, Cannot be powered on");
+                return;
+            }
+            System.out.println("Phone is turning on...");
+            isOn = true;
+            useBattery(1); 
+        }
+    }
+    // newContact: used to add new contact to arraylist of x object
+    // Wont work if phone is off
+    public void newContact(String name) {
+        if (!isOn) {
+            System.out.println("Phone is off. Cannot add contacts.");
+            return;
+        }
+        contacts.add(name);
+    }
+    // makeCall: main function of cellphone, used to makes calls to which ever contact what chosen and previously
+    // added to the conact list
+
+    // I couldnt figure out how to make the battery drain dynamically :(
+    // The plan was to make it so that you could stay on call as long as battery was available
+    public void makeCall(String contact, int minutes) {
+        if (!isOn) {
+            System.out.println("Phone is off, call cannot be made");
+            return;
+        }
+        if (!contacts.contains(contact)) {
+            System.out.println("Contact not found.");
+            return;
+        }
+        System.out.println("Calling " + contact + " for " + minutes + " minutes...");
+        useBattery(minutes * 0.5);
+    }
+
+    // toString method: Used to display all current attributes / values
+    public String toString() {
+        return "Brand: " + brand + "\nModel: " + model + "\nStorage: " + storage + "GB" + "\nPower: " + (isOn ? "On" : "Off") + "\nBattery: " + batteryLevel + "%" + "\nContacts: " + contacts;
+    }
+
+    // equals method: used to check if all current attribute / values are the same between two objects
+    public boolean equals(CellPhone other) {
+        return this.brand.equals(other.brand) && this.model.equals(other.model) && this.storage == other.storage && this.isOn == other.isOn && this.batteryLevel == other.batteryLevel && this.contacts.equals(other.contacts);
     }
 }
