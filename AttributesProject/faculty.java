bf902d3195e7a8c225d256ee900e4a925ecf26e2
Angel Perez <perryplatipus4@gmail.com>
2025-11-19 22:23:16 -0800

    Removed unused faculty class and added constructors to CellPhone class for better initialization options.

--- AttributesProject/faculty.java
+++
@@ -1,4 +0,0 @@
-package AttributesProject;
-public class faculty {
-    
-}
--- In-Class Labs/Week 12-13 In-Class Lab/CellPhone.java
+++ In-Class Labs/Week 12-13 In-Class Lab/CellPhone.java
@@ -1,11 +1,30 @@
 import java.util.ArrayList;
 
 public class CellPhone {
+    // All private variables
     private String brand;
     private String model;
     private int storage;
     private boolean isOn;
     private double batteryLevel;
     private ArrayList<String> contacts = new ArrayList<>();
-    
+
+    // Constructor 1: Default data constructor, initializes all variables when creating a new object
+    public CellPhone(){
+        brand = "Unknown";
+        model = "Unknown";
+        storage = 64;
+        isOn = false;
+        batteryLevel = 100;
+        contacts = new ArrayList<>();
+    }
+    // Constructor 2: Same as first constructor, just with parameters so that certain fields can be set by user 
+    public CellPhone(String brand, String model, int storage){
+        this.brand = brand;
+        this.model = model;
+        this.storage = storage;
+        isOn = false;
+        this.batteryLevel = 100;
+        contacts = new ArrayList<>();
+    }
 }
