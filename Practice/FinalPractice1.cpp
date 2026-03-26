a8827eadfddd89a76797344046ac563c2e049437
Angel Perez <perryplatipus4@gmail.com>
2026-03-26 15:22:18 -0700

    Add initial implementation of Main class and Sample.txt file

---
+++ Practice/FinalPractice1.cpp
@@ -0,0 +1,9 @@
+import java.io.FileInputStream;
+import java.io.IOExpception;
+
+public class Main {
+    public static void main(String[] args) {
+        FileInputStream input = new FileInputStream("Sample.txt");
+        
+    }
+}
